62bd247ab7e84a8e61231b62c6be8ebd65ac1595
tkrain23 <tk.rain09@gmail.com>
2018-10-07 17:28:44 -0700

    added kitsu api, updated jadge to pug, implemented image views

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
+var Kitsu = require('kitsu');
+var api = new Kitsu({baseUrl: 'https://kitsu.io/api/edge'});
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    api.get('/trending/anime')
+    .then(({ data }) => {
+        res.render('index', { title: 'Koshin', data: data });
+    })
+    .catch(err => {
+        console.log(err);
+    })
+
+
 });
 
 module.exports = router;
